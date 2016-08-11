15defc09ea6488fa59eed291fe3933ad8916469f
Mario Bonales <mbonales@touchcommerce.com>
2016-08-11 12:46:23 -0700

    Más comentarios

--- proyecto/js/perfiles.js
+++ proyecto/js/perfiles.js
@@ -7,6 +7,7 @@ var mapa = {
     hawkeye: "Hawkeye"
 };
 $(function () {
+   //Cuando se le de click a icono se ejecutará esta función
    $(".icon").click(function(event){
        //Mostrar la modal
        $(".modal, .modal-backdrop").show();
@@ -17,12 +18,12 @@ $(function () {
        //Reemplazar el nombre del heroe
        $(".heroe").text(mapa[id]);
    });
-
+   //Cuando se le de click al icono de cerrar o a la modal se ejecutará esta función
    $(".closeModal, .modal").click(function (event){
        //Esconder el modal
        $(".modal, .modal-backdrop").hide();
    });
-
+   //Cuando se le de click al elemento con clase modal-dialog se ejecutará esta función
    $(".modal-dialog").click(function (event){
        //Detiene la propagación del evento
        event.stopPropagation();
