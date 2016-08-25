a950be8bb3e537c21adcce4d60f0404ecaa15b54
Mario Bonales <mbonales@touchcommerce.com>
2016-08-25 11:52:33 -0700

    API Key y mongoose

--- proyecto/js/perfiles.js
+++ proyecto/js/perfiles.js
@@ -1,3 +1,5 @@
+/*global $*/
+//API KEY {apikey: "230e8bed4233bbebfaaadf119690dc1a"}
 var mapa = {
     thor: "Thor",
     nick: "Nick Fury",
@@ -7,25 +9,26 @@ var mapa = {
     hawkeye: "Hawkeye"
 };
 $(function () {
-   //Cuando se le de click a icono se ejecutará esta función
-   $(".icon").click(function(event){
-       //Mostrar la modal
-       $(".modal, .modal-backdrop").show();
-       //Prevenir el efecto deafult del enlace
-       event.preventDefault();
-       // Obtener el id del elemento al que se le dio click
-       var id = $(event.currentTarget).attr("id");
-       //Reemplazar el nombre del heroe
-       $(".heroe").text(mapa[id]);
-   });
-   //Cuando se le de click al icono de cerrar o a la modal se ejecutará esta función
-   $(".closeModal, .modal").click(function (event){
-       //Esconder el modal
-       $(".modal, .modal-backdrop").hide();
-   });
-   //Cuando se le de click al elemento con clase modal-dialog se ejecutará esta función
-   $(".modal-dialog").click(function (event){
-       //Detiene la propagación del evento
-       event.stopPropagation();
-   });
+    'use strict';
+    //Cuando se le de click a icono se ejecutará esta función
+    $(".icon").click(function (event) {
+        //Mostrar la modal
+        $(".modal, .modal-backdrop").show();
+        //Prevenir el efecto deafult del enlace
+        event.preventDefault();
+        // Obtener el id del elemento al que se le dio click
+        var id = $(event.currentTarget).attr("id");
+        //Reemplazar el nombre del heroe
+        $(".heroe").text(mapa[id]);
+    });
+    //Cuando se le de click al icono de cerrar o a la modal se ejecutará esta función
+    $(".closeModal, .modal").click(function () {
+        //Esconder el modal
+        $(".modal, .modal-backdrop").hide();
+    });
+    //Cuando se le de click al elemento con clase modal-dialog se ejecutará esta función
+    $(".modal-dialog").click(function (event) {
+        //Detiene la propagación del evento
+        event.stopPropagation();
+    });
 });
